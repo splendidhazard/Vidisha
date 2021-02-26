1e92a9e60a6d2eb1218f981a4db5f44b562484e4
splendidhazard <splendidhazard@gmail.com>
2021-02-26 18:54:30 +0530

    Created main function

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* * 
+ * A contact management and sharing application written in
+ * C++.
+ * Author: Prateek Sharma (splendidhazard@gmail.com)
+ * */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
